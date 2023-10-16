75d314ffe99710b0841f3d1589cf2d6beb1a91df
SurDno <surdnodiscordaccount129@yandex.ru>
2023-10-16 03:53:22 +0100

    Batch page fetching & loading

--- js/scout_contents.js
+++ js/scout_contents.js
@@ -1,11 +1,12 @@
-// Function to fetch a page and handle the response
+var link;
+
 async function fetchPage(pageNumber) {
 	try {
 		const response = await fetch(`${pageNumber}.html`);
 		if (!response.ok)
 			return;
 			
-		const link = document.createElement('a');
+		link = document.createElement('a');
 		link.href = `${pageNumber}.html`
 		link.innerText = `${pageNumber}`;
 		document.body.appendChild(link);
@@ -13,22 +14,14 @@ async function fetchPage(pageNumber) {
 	} catch (error) {}
 }
 
-/*
-// Loop through pages from 0 to 1000
-for (let pageNumber = 0; pageNumber <= 1000; pageNumber++) {
-	const formattedPageNumber = String(pageNumber).padStart(7, '0');
-	fetchPage(formattedPageNumber);
-	
-}
-*/
-
-function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 async function fetchPagesWithDelay() {
-  for (let pageNumber = 11000; pageNumber <= 12000; pageNumber++)
-    await fetchPage(String(pageNumber).padStart(7, '0'));
+    for (let i = 0; i <= 16000; i += 500) {
+        const promises = [];
+		
+		for (let pageNumber = i; pageNumber < i + 500; pageNumber++)
+			promises.push(fetchPage(String(pageNumber).padStart(7, '0')));
+		await Promise.all(promises);
+    }
 }
 
 fetchPagesWithDelay();
