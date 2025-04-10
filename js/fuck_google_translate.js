97614a1f80c96a37e3d368dab686de23216370d1
SurDno <surdno@yandex.ru>
2025-04-10 06:32:19 +0100

    What about time-based events

--- js/fuck_google_translate.js
+++ js/fuck_google_translate.js
@@ -1,3 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('md-dialog[type="alert"]').forEach(dialog => dialog.remove());
-});
+setInterval(() => {
+  const dialogs = document.querySelectorAll('md-dialog[type="alert"]');
+  console.log(`Found ${dialogs.length} alert dialog(s)`);
+  dialogs.forEach(dialog => { dialog.remove(); });
+}, 1000);
